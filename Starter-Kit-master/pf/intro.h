32e485eaa2db425ab5368b26f36cd2b8f7778897
AmjadRasheed7 <123481866+AmjadRasheed7@users.noreply.github.com>
2023-01-25 23:56:19 +0800

    Update intro.h

--- Starter-Kit-master/pf/intro.h
+++ Starter-Kit-master/pf/intro.h
@@ -12,20 +12,22 @@ class Intro{
     private:
     string choice_;
     int rows_, col_, zombie_;
+    bool changed_;
     vector<vector<char>> map_;
 
 public:
     int rows, col, zombie;
-    Intro(int rows = 5, int col = 9, int zombie = 1, string choice = "");
+    Intro(int rows = 5, int col = 9, int zombie = 1, string choice = "", bool changed = false);
     void changeSettings();
     void displayIntro();
-    void displayGame() const;
+    void displayGame();
     void mapinit(int rows, int col, int zombie);
-    void newBoard(Intro &intro, int rows, int col, int zombie);
+    void newBoard(Intro &intro, int rows, int col, int zombie, bool changed);
     void setObject(int col, int rows, char ch);
     int getRows() const;
     int getCol() const;
     int getZombie() const;
+    bool getChanged() const;
 };
 
 class Alien
@@ -39,5 +41,7 @@ public:
     Alien(int life = 100, int attack = 0, int range = 1);
     void alienPos(Intro &intro);
     void move(Intro &intro);
+    void alienDisplay(Intro &intro, Alien &alien);
+    void charAttri();
 
 };
