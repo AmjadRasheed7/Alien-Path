5087b7995c121ffe9bb9889416401dd277b4e0f8
MaherMMU <123481923+MaherMMU@users.noreply.github.com>
2023-02-09 03:06:26 +0800

    Update intro.h

--- Starter-Kit-master/pf/intro.h
+++ Starter-Kit-master/pf/intro.h
@@ -28,27 +28,45 @@ public:
     int getCol() const;
     int getZombie() const;
     bool getChanged() const;
+    bool isRock(int x, int y);
+    bool isZombie(int x, int y);
+    char getObject(int x, int y);
+    void removeTrail();
+    bool isArrow(int x, int y);
+    bool isHealth(int x, int y);
+    bool isPod(int x, int y);
+    
 };
-
 
 class Alien
 {
 private:
     int life_, attack_, range_, x_, y_, zombie2_;
+    bool redir_;
     char alien_; // 'A'
     string dir_; // up, down, left, right
-  int zombie_life[5] = {100, 150, 200, 250, 300};
+    int zombie_life[5] = {100, 150, 200, 250, 300};
     int zombie_attack[4] = {5, 10, 15, 20};
     int zombie_range[3] = {1, 2, 3};
-  int random_life, random_attack, random_range;
- 
+    int random_life, random_attack, random_range;
+
+
 public:
-  
     Alien(int life = 100, int attack = 0, int range = 0);
     void alienPos(Intro &intro);
     void move(Intro &intro);
     void alienDisplay(Intro &intro, Alien &alien);
-    void charAttri();
-     void randomAttri();
-  
+    void charAttri(Intro &intro);
+    void randomAttri(Intro &intro);
+    int newAlienPosX(Intro &intro);
+    int newAlienPosY(Intro &intro);
+    void alienUp(Intro &intro);
+    void alienLeft(Intro &intro);
+    void alienRight(Intro &intro);
+    void alienDown(Intro &intro);
+    void moveLeft(Intro &intro);
+    void moveRight(Intro &intro);
+    void moveUp(Intro &intro);
+    void moveDown(Intro &intro);
+    void changeArrow(Intro &intro);
 };
