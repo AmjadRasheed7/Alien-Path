577c6a6a26680a2756a2f0146cf0ee29e99fe06c
AmjadRasheed7 <123481866+AmjadRasheed7@users.noreply.github.com>
2023-02-09 03:06:30 +0800

    Update intro.h

--- Starter-Kit-master/pf/intro.h
+++ Starter-Kit-master/pf/intro.h
@@ -30,25 +30,25 @@ public:
     bool getChanged() const;
     bool isRock(int x, int y);
     bool isZombie(int x, int y);
-    bool isArrowUp(int x, int y);
-    bool isArrowDown(int x, int y);
-    bool isArrowLeft(int x, int y);
-    bool isArrowRight(int x, int y);
+    char getObject(int x, int y);
+    void removeTrail();
+    bool isArrow(int x, int y);
+    bool isHealth(int x, int y);
+    bool isPod(int x, int y);
+    
 };
 
 class Alien
 {
 private:
     int life_, attack_, range_, x_, y_, zombie2_;
+    bool redir_;
     char alien_; // 'A'
     string dir_; // up, down, left, right
-    
-    vector<int> zombie_life;
-    vector<int> zombie_attack;
-    vector<int> zombie_range;
-    vector<int> tempL;
-    vector<int> tempA;
-    vector<int> tempR;
+    int zombie_life[5] = {100, 150, 200, 250, 300};
+    int zombie_attack[4] = {5, 10, 15, 20};
+    int zombie_range[3] = {1, 2, 3};
+    int random_life, random_attack, random_range, count;
 
 
 public:
@@ -56,12 +56,18 @@ public:
     void alienPos(Intro &intro);
     void move(Intro &intro);
     void alienDisplay(Intro &intro, Alien &alien);
-    void charAttri();
-    int getZombieLife();
-    int getZombieRange();
-    int getZombieAttack();
-    int getLife();
-    int getAttack();
-    int newAlienPosX(Intro &into);
+    void charAttri(Intro &intro);
+    void randomAttri();
+    int newAlienPosX(Intro &intro);
     int newAlienPosY(Intro &intro);
+    void alienUp(Intro &intro);
+    void alienLeft(Intro &intro);
+    void alienRight(Intro &intro);
+    void alienDown(Intro &intro);
+    void moveLeft(Intro &intro);
+    void moveRight(Intro &intro);
+    void moveUp(Intro &intro);
+    void moveDown(Intro &intro);
+    void changeArrow(Intro &intro);
+    void zombiePos(Intro &intro);
 };
